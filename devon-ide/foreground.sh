a188a38fba1c10a2197f78eb2b875af4ca6909ac
Eduard Krieger <eduard.krieger@capgemini.com>
2021-03-11 13:42:50 +0100

    mit script

--- devon-ide/foreground.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Wait For Setup"; 
-
-while [ ! -f /home/finishedscript ] ;do sleep 2; done; 
- 
-echo "Done"
--- devon-ide/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Starting script"
-
-sudo apt-get -y purge --auto-remove nodejs
-
-touch /home/finishedscript
-echo "Finished" 
