e8875d2fdf622bb51959cf309142c9fb16666199
Eduard Krieger <eduard.krieger@capgemini.com>
2021-03-11 12:50:48 +0100

    mit script

--- devon-ide/setup.sh
+++ devon-ide/setup.sh
@@ -2,9 +2,9 @@
 
 echo "Starting script"
 
-sudo apt-get -y remove node.js
+sudo apt-get purge --auto-remove nodejs
 
-sudo apt-get -y remove git curl
+sudo apt-get -y pruge --auto-remove git curl
 
 
 echo "Finished"
