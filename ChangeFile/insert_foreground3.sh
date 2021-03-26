a4e4fb32ce30ecee00e3a021708959de5f4038ff
EduardKrieger <eduard.krieger@capgemini.com>
2021-03-26 15:02:53 +0100

    test

--- ChangeFile/insert_foreground3.sh
+++ ChangeFile/insert_foreground3.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-while [ grep -Fxq "##PLACEHOLDER##" some/Folder/For/test.txt ]; do sleep 2; done
+while [ grep -Fxq "##PLACEHOLDER##" some/Folder/For/test.txt ]
+    do 
+        sleep 2 
+        echo "Hallo"
+    done
