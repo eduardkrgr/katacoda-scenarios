f86cab8e3dc4120e30b078948b38b367279faad4
EduardKrieger <eduard.krieger@capgemini.com>
2021-03-26 16:15:52 +0100

    test

--- ChangeFile/insert_foreground4.sh
+++ ChangeFile/insert_foreground4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILE="/root/some/Folder/For/test.txt"
 PH="##PLACEHOLDER##"
-while [[ grep -Fxq "$PH" "$FILE" ]] ; do sleep 2; done
+while [ grep -Fxq "$PH" "$FILE" ] ; do sleep 2; done
