c8f6f988352eb869d753b3c579831a1b0119cf37
EduardKrieger <eduard.krieger@capgemini.com>
2021-03-30 15:37:23 +0200

    test

--- ChangeFile/insert_foreground3.sh
+++ ChangeFile/insert_foreground3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 FILE="/root/some/Folder/For/test.txt"
-PH="PLACEHOLDER"
-INSERTIONWORKED=$(sed -n 3p $FILE)
-while [ $INSERTIONWORKED != "$PH" ] ;
+PH="##PLACEHOLDER##"
+INSERTED=$(sed -n 3p $FILE)
+echo $INSERTED
+while [ $INSERTED != $PH ] ;
 do
     sleep 1
     echo "insertion not completed"
--- ChangeFile/insert_foreground4.sh
+++ ChangeFile/insert_foreground4.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 FILE="/root/some/Folder/For/test.txt"
-PH="PLACEHOLDER"
-INSERTIONWORKED=$(sed -n 1p $FILE)
-while [ $INSERTIONWORKED != "$PH" ] ;
+PH="##PLACEHOLDER##"
+INSERTED=$(sed -n 1p $FILE)
+echo $INSERTED
+while [ $INSERTED != $PH ] ;
 do
     sleep 1
     echo "insertion not completed"
